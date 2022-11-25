20417c2685d753094ebd5f979806ffcd2b13d2a8
Javier <javigar322@gmail.com>
2022-11-25 13:01:39 +0100

    update

--- src/config.mjs
+++ src/config.mjs
@@ -1,12 +1,12 @@
 export const SITE = {
-	name: 'AstroWind',
+	name: 'Pages',
 
 	origin: 'https://astrowind.vercel.app',
 	basePathname: '/',
 	trailingSlash: false,
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
-	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+	title: 'Xavigar Pages',
+	description: 'descripcion',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
 	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
